7b57c8dea3baf8d8c76d924150db1f33ed701394
GobuuriNaresh <nareshgobburi77@gmail.com>
2023-11-14 00:06:59 +0530

    creating first component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/ExpenseForm/NewExpense';
 const App = () => {
   const expenses = [
     {
@@ -35,7 +36,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses items={expenses}/>
     </div>
   );
