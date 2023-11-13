b2c4f3ab0fe4c4ec035f3b7aea1ee169765f325d
GobuuriNaresh <nareshgobburi77@gmail.com>
2023-11-13 23:17:21 +0530

    changeExpense to 100$

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,17 +7,24 @@ import './ExpenseItem.css'
 
 function ExpenseItem(props) {
 const [title, setTitle] =  useState(props.title);
+const [price, setPrice] = useState(props.price)
 
 
   const clickHandler = () => {
     setTitle('updated!!');
     console.log(title);
   }
+
+  const expenseHandler = () => {
+    setPrice('$100');
+    console.log(price);
+  }  
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
-      <ExpenseDetails price={props.price} location={props.location} title={title}/>
+      <ExpenseDetails price={price} location={props.location} title={title}/>
       <button onClick={clickHandler}>Change Title</button>
+      <button onClick={expenseHandler}>Change Price</button>
     </Card>
   );
 }
